c66b30d11f098e073d7f649fb2ff31ff14c9229e
Pranay Reddy <apranay941@gmail.com>
2024-11-05 10:21:07 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,8 @@
+p1=input("your turn (rock, paper, scissors)")
+p2=input("your turn (rock, paper, scissors)")
+if p1==p2:
+    print("TIE")
+elif(p1 == 'rock' and p2 == 'scissors' ) or (p1 == 'scissors' and p2 =='paper') or (p1 =='paper' and p2 =='rock'):
+    print("p1 wins")
+else:
+    print("p2 wins")
